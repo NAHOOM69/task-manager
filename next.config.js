51b5fe61b65b85a8039e18e8999305d0be7ca414
NAHOOM69 <139867592+NAHOOM69@users.noreply.github.com>
2024-11-19 14:40:16 +0200

    Add Next.js configuration files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
-  // נוסיף את השורה הזו לביטול בדיקות טיפוסים בזמן בנייה
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
